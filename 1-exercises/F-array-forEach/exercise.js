7d68fbe94f60924292d8c1764ce8c8b38e572878
Anza-Azam <anzaazam.nw4@gmail.com>
2021-07-03 16:26:50 -0700

    Updated F

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -9,21 +9,25 @@
 
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 function ifMultiples(number){
-if (number%3===0)
-return 'Fizz';
-else if (number%5===0)
-return 'Buzz';
-else if (number%3===0&&number%5===0)
-return 'FizzBuzz';
+  if (number % 3 === 0 && number % 5 === 0)
+    return 'FizzBuzz';
+  else if (number % 3 === 0)
+    return 'Fizz';
+  else if (number % 5 === 0)
+    return 'Buzz';
+  else return number;
+
 }
 
-function a(name,index){
-  return name,index;
-  
+function log(name) { 
+	console.log(name);
 }
-var result=arr.map(ifMultiples).forEach(a);
 
-console.log(result);
+const namesFormatted = arr.map(ifMultiples);
+namesFormatted.forEach(log);
+
+
+
 /* EXPECTED OUTPUT */
 
 /*
